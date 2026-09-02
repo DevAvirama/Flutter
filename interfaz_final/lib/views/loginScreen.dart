4886c6af5eef781487aef3552255422e80731744
DevAvirama <santyaviramagnf@gmail.com>
2026-09-02 16:41:36 -0500

    Listado

--- interfaz_final/lib/views/loginScreen.dart
+++ interfaz_final/lib/views/loginScreen.dart
@@ -11,129 +11,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final List<Map<String, dynamic>> listaDeAutos = [
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=1',
-      'marca': 'Toyota',
-      'modelo': 'Corolla',
-      'anio': '2022',
-      'disponibilidad': 1,
-      'precio': '99.99',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=2',
-      'marca': 'Nissan',
-      'modelo': 'Sentra',
-      'anio': '2021',
-      'disponibilidad': 1,
-      'precio': '89.99',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=3',
-      'marca': 'Ford',
-      'modelo': 'Mustang',
-      'anio': '2023',
-      'disponibilidad': 0,
-      'precio': '199.99',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=4',
-      'marca': 'Chevrolet',
-      'modelo': 'Camaro',
-      'anio': '2020',
-      'disponibilidad': 1,
-      'precio': '150.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=5',
-      'marca': 'Mazda',
-      'modelo': 'CX-5',
-      'anio': '2023',
-      'disponibilidad': 1,
-      'precio': '110.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=6',
-      'marca': 'Hyundai',
-      'modelo': 'Tucson',
-      'anio': '2022',
-      'disponibilidad': 0,
-      'precio': '105.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=7',
-      'marca': 'Kia',
-      'modelo': 'Sportage',
-      'anio': '2021',
-      'disponibilidad': 1,
-      'precio': '95.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=8',
-      'marca': 'Volkswagen',
-      'modelo': 'Jetta',
-      'anio': '2022',
-      'disponibilidad': 1,
-      'precio': '100.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=9',
-      'marca': 'Honda',
-      'modelo': 'Civic',
-      'anio': '2023',
-      'disponibilidad': 1,
-      'precio': '115.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=10',
-      'marca': 'BMW',
-      'modelo': 'Serie 3',
-      'anio': '2024',
-      'disponibilidad': 0,
-      'precio': '220.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=11',
-      'marca': 'Mercedes-Benz',
-      'modelo': 'Clase C',
-      'anio': '2023',
-      'disponibilidad': 1,
-      'precio': '230.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=12',
-      'marca': 'Audi',
-      'modelo': 'A4',
-      'anio': '2022',
-      'disponibilidad': 1,
-      'precio': '210.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=13',
-      'marca': 'Renault',
-      'modelo': 'Duster',
-      'anio': '2021',
-      'disponibilidad': 1,
-      'precio': '80.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=14',
-      'marca': 'Jeep',
-      'modelo': 'Renegade',
-      'anio': '2022',
-      'disponibilidad': 0,
-      'precio': '130.00',
-    },
-    {
-      'imageUrl': 'https://picsum.photos/330/200?random=15',
-      'marca': 'Suzuki',
-      'modelo': 'Vitara',
-      'anio': '2023',
-      'disponibilidad': 1,
-      'precio': '90.00',
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -200,7 +77,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => MenuPrincipal(autos: listaDeAutos),
+                    builder: (context) => const MenuPrincipal(),
                   ),
                 );
               },
--- interfaz_final/lib/views/menuPrincipal.dart
+++ interfaz_final/lib/views/menuPrincipal.dart
@@ -3,16 +3,144 @@ import 'package:interfaz_final/theme/app_colors.dart';
 import 'package:interfaz_final/views/detalleVehiculo.dart';
 import 'package:interfaz_final/views/menuDrawer.dart';
 
-class MenuPrincipal extends StatelessWidget {
-  final List<Map<String, dynamic>> autos;
+class MenuPrincipal extends StatefulWidget {
+  const MenuPrincipal({super.key});
 
-  MenuPrincipal({super.key, required this.autos});
+  @override
+  State<MenuPrincipal> createState() => _MenuPrincipalState();
+}
+
+class _MenuPrincipalState extends State<MenuPrincipal> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, dynamic>> listaDeAutos = [
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=1',
+      'marca': 'Toyota',
+      'modelo': 'Corolla',
+      'anio': '2022',
+      'disponibilidad': 1,
+      'precio': '99.99',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=2',
+      'marca': 'Nissan',
+      'modelo': 'Sentra',
+      'anio': '2021',
+      'disponibilidad': 1,
+      'precio': '89.99',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=3',
+      'marca': 'Ford',
+      'modelo': 'Mustang',
+      'anio': '2023',
+      'disponibilidad': 0,
+      'precio': '199.99',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=4',
+      'marca': 'Chevrolet',
+      'modelo': 'Camaro',
+      'anio': '2020',
+      'disponibilidad': 1,
+      'precio': '150.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=5',
+      'marca': 'Mazda',
+      'modelo': 'CX-5',
+      'anio': '2023',
+      'disponibilidad': 1,
+      'precio': '110.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=6',
+      'marca': 'Hyundai',
+      'modelo': 'Tucson',
+      'anio': '2022',
+      'disponibilidad': 0,
+      'precio': '105.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=7',
+      'marca': 'Kia',
+      'modelo': 'Sportage',
+      'anio': '2021',
+      'disponibilidad': 1,
+      'precio': '95.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=8',
+      'marca': 'Volkswagen',
+      'modelo': 'Jetta',
+      'anio': '2022',
+      'disponibilidad': 1,
+      'precio': '100.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=9',
+      'marca': 'Honda',
+      'modelo': 'Civic',
+      'anio': '2023',
+      'disponibilidad': 1,
+      'precio': '115.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=10',
+      'marca': 'BMW',
+      'modelo': 'Serie 3',
+      'anio': '2024',
+      'disponibilidad': 0,
+      'precio': '220.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=11',
+      'marca': 'Mercedes-Benz',
+      'modelo': 'Clase C',
+      'anio': '2023',
+      'disponibilidad': 1,
+      'precio': '230.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=12',
+      'marca': 'Audi',
+      'modelo': 'A4',
+      'anio': '2022',
+      'disponibilidad': 1,
+      'precio': '210.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=13',
+      'marca': 'Renault',
+      'modelo': 'Duster',
+      'anio': '2021',
+      'disponibilidad': 1,
+      'precio': '80.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=14',
+      'marca': 'Jeep',
+      'modelo': 'Renegade',
+      'anio': '2022',
+      'disponibilidad': 0,
+      'precio': '130.00',
+    },
+    {
+      'imageUrl': 'https://picsum.photos/330/200?random=15',
+      'marca': 'Suzuki',
+      'modelo': 'Vitara',
+      'anio': '2023',
+      'disponibilidad': 1,
+      'precio': '90.00',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.fondo,
-      drawer: MenuDrawerPerfil(),
+      drawer: const MenuDrawerPerfil(),
       appBar: AppBar(
         title: const Text('Alquiler de Vehículos'),
         backgroundColor: AppColors.encabezado,
@@ -39,12 +167,12 @@ class MenuPrincipal extends StatelessWidget {
             ),
             const SizedBox(height: 16.0),
             Expanded(
-              child: autos.isEmpty
+              child: listaDeAutos.isEmpty
                   ? const Center(child: Text('No hay vehículos para mostrar'))
                   : ListView.builder(
-                      itemCount: autos.length,
+                      itemCount: listaDeAutos.length,
                       itemBuilder: (context, index) {
-                        final auto = autos[index];
+                        final auto = listaDeAutos[index];
                         return ListTile(
                           leading: ClipRRect(
                             borderRadius: BorderRadius.circular(8),
@@ -72,9 +200,7 @@ class MenuPrincipal extends StatelessWidget {
                           ),
                           subtitle: Text(
                             'Año: ${auto['anio']}\nPrecio: \$${auto['precio']}/día',
-                            style: TextStyle(
-                              color: AppColors.texto.withOpacity(0.8),
-                            ),
+                            style: TextStyle(color: AppColors.texto),
                           ),
                           trailing: Icon(
                             Icons.arrow_forward_ios,
@@ -105,8 +231,14 @@ class MenuPrincipal extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         selectedItemColor: AppColors.boton,
-        unselectedItemColor: AppColors.texto.withOpacity(0.6),
+        unselectedItemColor: AppColors.texto,
         showUnselectedLabels: true,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Inicio'),
           BottomNavigationBarItem(
